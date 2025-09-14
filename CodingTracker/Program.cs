8a8ac849f3d611408aed7aa83c62f245ce621abc
Anmol <anmol1314@hotmail.com>
2025-09-14 05:10:37 -0700

    testing github correct push account

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Anmol. Welcome to CSharp!");
-Console.WriteLine("This is your coding tracker application.");
+Console.WriteLine("This is your coding tracker application..");
